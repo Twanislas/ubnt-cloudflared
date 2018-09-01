96e3bf2a14dd6fec56ea34f35eff86117677b73f
Antoine Rahier <antoine.rahier@gmail.com>
2018-09-01 14:45:57 +0200

    Install script

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -e
+
+echo "Installing cloudflared..."
+sudo /usr/bin/curl --fail https://raw.githubusercontent.com/Twanislas/ubnt-cloudflared/master/cloudflared-setup.sh --output /config/scripts/post-config.d/cloudflared-setup.sh
+sudo /bin/chmod +x /config/scripts/post-config.d/cloudflared-setup.sh
+sudo /config/scripts/post-config.d/cloudflared-setup.sh
+echo "Done !"
