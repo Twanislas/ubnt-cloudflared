eb237544b3bfb3b7cdc0fd020b9210cd8e8a6ce6
Antoine Rahier <antoine.rahier@gmail.com>
2018-09-02 00:52:34 +0200

    No need for sudo and better file replacement

--- cloudflared-setup.sh
+++ cloudflared-setup.sh
@@ -2,12 +2,18 @@
 source /opt/vyatta/etc/functions/script-template
 
 # Pull files
-sudo mkdir -p /etc/cloudflared
-if [ ! -f /etc/cloudflared/config.yml ] || [ "$1" = "pull" ]; then sudo /usr/bin/curl -sf https://raw.githubusercontent.com/Twanislas/ubnt-cloudflared/master/config.yml --output /etc/cloudflared/config.yml; fi
-if [ ! -f /usr/local/bin/cloudflared ] || [ "$1" = "pull" ]; then sudo /usr/bin/curl -sf https://raw.githubusercontent.com/Twanislas/ubnt-cloudflared/master/cloudflared-$(uname -m) --output /usr/local/bin/cloudflared; fi
-sudo /bin/chmod +x /usr/local/bin/cloudflared
-sudo /usr/local/bin/cloudflared service install
-sudo /etc/init.d/cloudflared restart
+mkdir -p /etc/cloudflared
+if [ ! -f /etc/cloudflared/config.yml ] || [ "$1" = "pull" ]; then;
+	rm /etc/cloudflared/config.yml
+	/usr/bin/curl -sf https://raw.githubusercontent.com/Twanislas/ubnt-cloudflared/master/config.yml --output /etc/cloudflared/config.yml
+fi
+if [ ! -f /usr/local/bin/cloudflared ] || [ "$1" = "pull" ]; then;
+	rm /usr/local/bin/cloudflared
+	sudo /usr/bin/curl -sf https://raw.githubusercontent.com/Twanislas/ubnt-cloudflared/master/cloudflared-$(uname -m) --output /usr/local/bin/cloudflared
+fi
+/bin/chmod +x /usr/local/bin/cloudflared
+/usr/local/bin/cloudflared service install
+/etc/init.d/cloudflared restart
 
 # Configure the system to use local DNS proxy
 configure
