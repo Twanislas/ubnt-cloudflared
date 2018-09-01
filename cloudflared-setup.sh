9a7c87525d1e65b4b5d3d930234e5d10cce2b43a
Antoine Rahier <antoine.rahier@gmail.com>
2018-09-02 00:45:50 +0200

    Better sudo

--- cloudflared-setup.sh
+++ cloudflared-setup.sh
@@ -3,8 +3,8 @@ source /opt/vyatta/etc/functions/script-template
 
 # Pull files
 sudo mkdir -p /etc/cloudflared
-sudo if [ ! -f /etc/cloudflared/config.yml ] || [ "$1" = "pull" ]; then /usr/bin/curl -sf https://raw.githubusercontent.com/Twanislas/ubnt-cloudflared/master/config.yml --output /etc/cloudflared/config.yml; fi
-sudo if [ ! -f /usr/local/bin/cloudflared ] || [ "$1" = "pull" ]; then /usr/bin/curl -sf https://raw.githubusercontent.com/Twanislas/ubnt-cloudflared/master/cloudflared-$(uname -m) --output /usr/local/bin/cloudflared; fi
+if [ ! -f /etc/cloudflared/config.yml ] || [ "$1" = "pull" ]; then sudo /usr/bin/curl -sf https://raw.githubusercontent.com/Twanislas/ubnt-cloudflared/master/config.yml --output /etc/cloudflared/config.yml; fi
+if [ ! -f /usr/local/bin/cloudflared ] || [ "$1" = "pull" ]; then sudo /usr/bin/curl -sf https://raw.githubusercontent.com/Twanislas/ubnt-cloudflared/master/cloudflared-$(uname -m) --output /usr/local/bin/cloudflared; fi
 sudo /bin/chmod +x /usr/local/bin/cloudflared
 sudo /usr/local/bin/cloudflared service install
 sudo /etc/init.d/cloudflared restart
