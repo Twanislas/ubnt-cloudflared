7fade8be926d9021dc3be966a33f4acc4661093b
Antoine Rahier <antoine.rahier@gmail.com>
2018-09-02 00:53:55 +0200

    Fix rm missing -f

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "Installing cloudflared"
-sudo rm /config/scripts/post-config.d/cloudflared-setup.sh
+sudo rm -f /config/scripts/post-config.d/cloudflared-setup.sh
 sudo /usr/bin/curl -sf https://raw.githubusercontent.com/Twanislas/ubnt-cloudflared/master/cloudflared-setup.sh --output /config/scripts/post-config.d/cloudflared-setup.sh
 sudo /bin/chmod +x /config/scripts/post-config.d/cloudflared-setup.sh
 sudo /config/scripts/post-config.d/cloudflared-setup.sh pull
