ee29f6e9cd2b9aa120bbe69cf2374a0a833a7baa
Antoine Rahier <antoine.rahier@gmail.com>
2018-09-01 19:07:22 +0200

    Small |UX improvements

--- install.sh
+++ install.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 set -e
 
-echo "Installing cloudflared..."
+echo "Installing cloudflared"
 sudo /usr/bin/curl --fail https://raw.githubusercontent.com/Twanislas/ubnt-cloudflared/master/cloudflared-setup.sh --output /config/scripts/post-config.d/cloudflared-setup.sh
 sudo /bin/chmod +x /config/scripts/post-config.d/cloudflared-setup.sh
 sudo /config/scripts/post-config.d/cloudflared-setup.sh
-echo "Done !"
