29303478ad9c89ef98549d1a62f36dfbadb609c4
Antoine Rahier <antoine.rahier@gmail.com>
2018-09-02 09:53:24 +0200

    Cleanup logs as well

--- uninstall.sh
+++ uninstall.sh
@@ -4,7 +4,11 @@ source /opt/vyatta/etc/functions/script-template
 # Stop cloudflared and remove files
 sudo /etc/init.d/cloudflared stop
 sudo /usr/local/bin/cloudflared service uninstall
-sudo rm -rf /etc/cloudflared /usr/local/bin/cloudflared /config/scripts/post-config.d/cloudflared-setup.sh
+sudo rm -rf \
+	/etc/cloudflared \
+	/usr/local/bin/cloudflared \
+	/config/scripts/post-config.d/cloudflared-setup.sh \
+	/var/log/cloudflared*
 
 # Reset default DNS config
 configure
