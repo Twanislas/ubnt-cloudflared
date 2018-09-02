e9e249429d4796d8978e86a3644ac98c318cb0e9
Antoine Rahier <antoine.rahier@gmail.com>
2018-09-02 15:07:52 +0200

    No need for those rm's

--- cloudflared-setup.sh
+++ cloudflared-setup.sh
@@ -4,11 +4,9 @@ source /opt/vyatta/etc/functions/script-template
 # Pull files
 mkdir -p /etc/cloudflared
 if [ ! -f /etc/cloudflared/config.yml ] || [ "$1" = "pull" ]; then
-	rm -f /etc/cloudflared/config.yml
 	/usr/bin/curl -sf https://raw.githubusercontent.com/Twanislas/ubnt-cloudflared/master/config.yml --output /etc/cloudflared/config.yml
 fi
 if [ ! -f /usr/local/bin/cloudflared ] || [ "$1" = "pull" ]; then
-	rm -f /usr/local/bin/cloudflared
 	sudo /usr/bin/curl -sf https://raw.githubusercontent.com/Twanislas/ubnt-cloudflared/master/cloudflared --output /usr/local/bin/cloudflared
 fi
 /bin/chmod +x /usr/local/bin/cloudflared
